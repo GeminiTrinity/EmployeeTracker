2e0f94231749383ae962687ad7ef914b77f14bfc
Jake Krueger <jmk4u14@gmail.com>
2020-12-17 00:16:26 -0500

    Added questions needed to add, remove, and update employees

--- server.js
+++ server.js
@@ -9,3 +9,110 @@ var connection = mysql.createConnection({
     password: "TheDoctor14!",
     database: "employee_db"
 })
+
+const firstQuestion = [
+    {
+        name: "intro",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employeees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager"
+        ]
+    }
+];
+
+const addEmployeeQuestion = [
+    {
+        name: "first-name",
+        type: "input",
+        message: "What is the Employee's first name?"
+    },
+    {
+        name: "last-name",
+        type: "input",
+        message: "What is the employee's last name?"
+    },
+    {
+        name: "employee-role",
+        type: "list",
+        message: "What is the employee's role?",
+        choices: [
+            "Sales Lead",
+            "Salesperson",
+            "Lead Engineer",
+            "Software Engineer",
+            "Account Manager",
+            "Accountant",
+            "Legal Team Lead"
+        ]
+    },
+    {
+        name: "employee-manager",
+        type: "list",
+        message: "Who is the employee's manager?",
+        choices: [
+
+        ]
+    },
+];
+
+const removeEmployee = [
+    {
+        name: "remove-employee",
+        type: "list",
+        message: "Which employee would you like to remove?",
+        choices: [
+
+        ]
+    }
+]
+
+const updateEmployeeRole = [
+    {
+        name: "update-employee-role",
+        type: "list",
+        message: "Which employee do you want to update?",
+        choices: [
+
+        ]
+    },
+    {
+        name: "new-role",
+        type: "list",
+        message: "What is the employee's new role?",
+        choices: [
+            "Sales Lead",
+            "Salesperson",
+            "Lead Engineer",
+            "Software Engineer",
+            "Account Manager",
+            "Accountant",
+            "Legal Team Lead"
+        ]
+    }
+];
+
+const updateEmployeeManager = [
+    {
+        name: "update-employee-manager",
+        type: "list",
+        message: "Which employee do you want to update?",
+        choices: [
+
+        ]
+    },
+    {
+        name: "new-manager",
+        type: "list",
+        message: "Who is the employee's new manager?",
+        choices: [
+
+        ]
+    }
+];
