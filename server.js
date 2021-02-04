0ea198aeaed24988fd7a63d3e8c798a0c918f6c8
Jake Krueger <jmk4u14@gmail.com>
2021-02-03 21:41:57 -0500

    Separated connection from server.js file

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
-var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "TheDoctor14!",
-    database: "employee_db"
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected as ID: " + connection.threadId);
-})
 
 function askFirstQuestion() {
     inquirer.prompt(firstQuestion).then((answer) => {
